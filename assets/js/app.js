6dd2fd2c3f9961befcf87f158b04f37802021669
joelsilas1816 <silasjoel03@gmail.com>
2021-11-11 20:57:12 +0530

    Child

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,3 +1,30 @@
-console.log("Message");
-console.warn("Warning");
-console.error("HO");
+// console.log("Message");
+// console.warn("Warning");
+// console.error("HO");
+
+// var a = ["abc" , "good" , 1 , -3.12]
+
+// console.log("Hello " + typeof a);
+
+var arr=["one","two","three"]
+
+var list = document.querySelector("#uni")
+
+for (var i = 0; i<arr.length ; i++){
+    var listitem = document.createElement('li');
+    var txt = document.createElement(arr[i]);
+    listitem.appendChild(txt);
+//     list.appendChild(listitem);
+}
+
+console.log(arr)
+console.log(list)
+console.log(listitem)
+console.log(txt)
+// console.log(typeof arr)
+// console.log(typeof list)
+// console.log(typeof listitem)
+// console.log(typeof txt)
+
+
+
