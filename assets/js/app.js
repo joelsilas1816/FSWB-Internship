4bf3d0733956d6c65c1e1315a67ebd554ca51849
joelsilas1816 <silasjoel03@gmail.com>
2021-11-11 22:49:42 +0530

    Fucntions

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,32 +1,14 @@
-// console.log("Message");
-// console.warn("Warning");
-// console.error("HO");
-
-// var a = ["abc" , "good" , 1 , -3.12]
-
-// console.log("Hello " + typeof a);
-
-var arr=["one","two","three"]
-
-var list = document.getElementById("uni")
-
-for (var i = 0; i<arr.length ; i++){
-    var listitem = document.createElement('li');
-    var txt = document.createTextNode(arr[i]);
-    // console.log("Hello",arr[i])
-    listitem.appendChild(txt);
-    // console.log(listitem)
-    list.appendChild(listitem);
+var c = () =>{
+    console.log("arrow function")
 }
 
-// console.log(arr)
-// console.log(list)
-// console.log(listitem)
-// console.log(txt)
-// console.log(typeof arr)
-// console.log(typeof list)
-// console.log(typeof listitem)
-// console.log(typeof txt)
+var c = function() {
+    console.log("expression")
+}
 
+function c(){
+    console.log("keyword declaration");
+}
 
-
+console.log("Program")
+c();
